778c371b95855167488f31c22999b303ba779ddf
root <christopher.klammer@gmail.com>
2023-04-23 20:21:58 +0000

    Applying stash

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
+echo "Configuring and building Thirdparty/DBoW3 ..."
 
-cd Thirdparty/DBoW2
+cd Thirdparty/DBoW3
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j2
 
 cd ../../g2o
 
@@ -13,19 +13,13 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j2
 
 cd ../../../
-
-echo "Uncompress vocabulary ..."
-
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_CUDNN=1
+make -j2
