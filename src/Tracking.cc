36e7b4e11dd700bcc2cc429ba1f065c0566ab31d
klammecr <christopher.klammer@gmail.com>
2023-04-25 17:04:35 -0400

    NN only matching

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -765,7 +765,8 @@ bool Tracking::TrackReferenceKeyFrame()
     ORBmatcher matcher(0.7,true);
     std::vector<MapPoint*> vpMapPointMatches;
 
-    int nmatches = matcher.SearchByBoW(mpReferenceKF,mCurrentFrame,vpMapPointMatches);
+    //int nmatches = matcher.SearchByBoW(mpReferenceKF,mCurrentFrame,vpMapPointMatches);
+    int nmatches = matcher.SearchByNN(mpReferenceKF,mCurrentFrame,vpMapPointMatches);
 
     if(nmatches<15)
         return false;
@@ -1183,13 +1184,15 @@ void Tracking::SearchLocalPoints()
     if(nToMatch>0)
     {
         ORBmatcher matcher(0.8);
-        int th = 1;
-        if(mSensor==System::RGBD)
-            th=3;
-        // If the camera has been relocalised recently, perform a coarser search
-        if(mCurrentFrame.mnId<mnLastRelocFrameId+2)
-            th=5;
-        matcher.SearchByProjection(mCurrentFrame,mvpLocalMapPoints,th);
+        // int th = 1;
+        // if(mSensor==System::RGBD)
+        //     th=3;
+        // // If the camera has been relocalised recently, perform a coarser search
+        // if(mCurrentFrame.mnId<mnLastRelocFrameId+2)
+        //     th=5;
+        //matcher.SearchByProjection(mCurrentFrame,mvpLocalMapPoints,th);
+        // NN only matching
+        matcher.SearchByNN(mCurrentFrame,mvpLocalMapPoints);  
     }
 }
 
